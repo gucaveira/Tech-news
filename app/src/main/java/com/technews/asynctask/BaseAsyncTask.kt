d30648faaf74567fb32a2b6b05c2a9e7674b5e9c
gustavo <pereiragustavo251>
2020-01-15 15:53:35 -0300

    copiando classe e packs do projeto base

---
+++ app/src/main/java/com/technews/asynctask/BaseAsyncTask.kt
@@ -0,0 +1,16 @@
+package com.technews.asynctask
+
+import android.os.AsyncTask
+
+class BaseAsyncTask<T>(
+    private val quandoExecuta: () -> T,
+    private val quandoFinaliza: (resultado: T) -> Unit
+) : AsyncTask<Void, Void, T>() {
+
+    override fun doInBackground(vararg params: Void?) = quandoExecuta()
+
+    override fun onPostExecute(resultado: T) {
+        super.onPostExecute(resultado)
+        quandoFinaliza(resultado)
+    }
+}
---
+++ app/src/main/java/com/technews/ui/activity/VisualizaNoticiaActivity.kt
@@ -0,0 +1,92 @@
+package com.technews.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.technews.R
+
+import com.technews.database.AppDatabase
+import com.technews.model.Noticia
+import com.technews.repository.NoticiaRepository
+import com.technews.ui.activity.extensions.mostraErro
+import kotlinx.android.synthetic.main.activity_visualiza_noticia.*
+
+private const val NOTICIA_NAO_ENCONTRADA = "Notícia não encontrada"
+private const val TITULO_APPBAR = "Notícia"
+private const val MENSAGEM_FALHA_REMOCAO = "Não foi possível remover notícia"
+
+class VisualizaNoticiaActivity : AppCompatActivity() {
+
+    private val noticiaId: Long by lazy {
+        intent.getLongExtra(NOTICIA_ID_CHAVE, 0)
+    }
+    private val repository by lazy {
+        NoticiaRepository(AppDatabase.getInstance(this).noticiaDAO)
+    }
+    private lateinit var noticia: Noticia
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visualiza_noticia)
+        title = TITULO_APPBAR
+        verificaIdDaNoticia()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        buscaNoticiaSelecionada()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.visualiza_noticia_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.visualiza_noticia_menu_edita -> abreFormularioEdicao()
+            R.id.visualiza_noticia_menu_remove -> remove()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun buscaNoticiaSelecionada() {
+        repository.buscaPorId(noticiaId, quandoSucesso = { noticiaEncontrada ->
+            noticiaEncontrada?.let {
+                this.noticia = it
+                preencheCampos(it)
+            }
+        })
+    }
+
+    private fun verificaIdDaNoticia() {
+        if (noticiaId == 0L) {
+            mostraErro(NOTICIA_NAO_ENCONTRADA)
+            finish()
+        }
+    }
+
+    private fun preencheCampos(noticia: Noticia) {
+        activity_visualiza_noticia_titulo.text = noticia.titulo
+        activity_visualiza_noticia_texto.text = noticia.texto
+    }
+
+    private fun remove() {
+        if (::noticia.isInitialized) {
+            repository.remove(noticia, quandoSucesso = {
+                finish()
+            }, quandoFalha = {
+                mostraErro(MENSAGEM_FALHA_REMOCAO)
+            })
+        }
+    }
+
+    private fun abreFormularioEdicao() {
+        val intent = Intent(this, FormularioNoticiaActivity::class.java)
+        intent.putExtra(NOTICIA_ID_CHAVE, noticiaId)
+        startActivity(intent)
+    }
+
+}
