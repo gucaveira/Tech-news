979c5f71f0536c05c88ba5c6faffe9b89f392fcc
Gustavo <pereiragustavo215@gmail.com>
2020-03-22 18:16:36 -0300

    Iniciando com Fragment

--- app/src/main/java/com/technews/ui/activity/ListaNoticiasActivity.kt
+++ app/src/main/java/com/technews/ui/activity/ListaNoticiasActivity.kt
@@ -1,78 +1,20 @@
 package com.technews.ui.activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
 import com.technews.R
-import com.technews.database.AppDatabase
 import com.technews.model.Noticia
-import com.technews.repository.NoticiaRepository
-import com.technews.ui.activity.extensions.mostraErro
-import com.technews.ui.recyclerview.adapter.ListaNoticiasAdapter
-import com.technews.ui.viewmodel.ListaNoticiasViewModel
-import com.technews.ui.viewmodel.factory.ListaNoticiasViewModelFactory
-import kotlinx.android.synthetic.main.activity_lista_noticias.*
 
 private const val TITULO_APPBAR = "Notícias"
-private const val MENSAGEM_FALHA_CARREGAR_NOTICIAS = "Não foi possível carregar as novas notícias"
 
 class ListaNoticiasActivity : AppCompatActivity() {
-
-    private val adapter by lazy {
-        ListaNoticiasAdapter(context = this)
-    }
-
-    private val viewModel by lazy {
-        val repository = NoticiaRepository(AppDatabase.getInstance(this).noticiaDAO)
-        val factory = ListaNoticiasViewModelFactory(repository)
-        val provedor = ViewModelProviders.of(this, factory)
-        provedor.get(ListaNoticiasViewModel::class.java)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_noticias)
         title = TITULO_APPBAR
-        configuraRecyclerView()
-        configuraFabAdicionaNoticia()
-
-        Log.i("viewModel", viewModel.toString())
-    }
-
-    override fun onResume() {
-        super.onResume()
-        buscaNoticias()
-    }
-
-    private fun configuraFabAdicionaNoticia() {
-        activity_lista_noticias_fab_salva_noticia.setOnClickListener {
-            abreFormularioModoCriacao()
-        }
-    }
-
-    private fun configuraRecyclerView() {
-        val divisor = DividerItemDecoration(this, VERTICAL)
-        activity_lista_noticias_recyclerview.addItemDecoration(divisor)
-        activity_lista_noticias_recyclerview.adapter = adapter
-        configuraAdapter()
-    }
-
-    private fun configuraAdapter() {
-        adapter.quandoItemClicado = this::abreVisualizadorNoticia
-    }
-
-    private fun buscaNoticias() {
-        viewModel.buscaTodos().observe(this, Observer { resources ->
-            resources.dado?.let { adapter.atualiza(it) }
-            resources.erro?.let {
-                mostraErro(MENSAGEM_FALHA_CARREGAR_NOTICIAS)
-            }
-        })
     }
 
     private fun abreFormularioModoCriacao() {
@@ -85,4 +27,7 @@ class ListaNoticiasActivity : AppCompatActivity() {
         intent.putExtra(NOTICIA_ID_CHAVE, it.id)
         startActivity(intent)
     }
+
+    fun AbreIntent(context: Context ){}
+
 }
