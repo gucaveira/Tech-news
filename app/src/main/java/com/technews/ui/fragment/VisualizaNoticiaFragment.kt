e0f5308e2d9c2f9e58ee885e9c2c75a56117c712
Gustavo <pereiragustavo215@gmail.com>
2020-03-28 19:21:56 -0300

    Criando o fragment de maneira programavel

---
+++ app/src/main/java/com/technews/ui/fragment/VisualizaNoticiaFragment.kt
@@ -0,0 +1,88 @@
+package com.technews.ui.fragment
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.technews.R
+import com.technews.model.Noticia
+import com.technews.ui.activity.NOTICIA_ID_CHAVE
+import com.technews.ui.fragment.extensions.mostraErro
+import com.technews.ui.viewmodel.VisualizaNoticiaViewModel
+import kotlinx.android.synthetic.main.fragment_visualiza_noticia.*
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
+
+private const val NOTICIA_NAO_ENCONTRADA = "Notícia não encontrada"
+private const val MENSAGEM_FALHA_REMOCAO = "Não foi possível remover notícia"
+
+class VisualizaNoticiaFragment : Fragment() {
+
+    private val noticiaId: Long by lazy {
+        arguments?.getLong(NOTICIA_ID_CHAVE)
+            ?: throw IllegalArgumentException("Id inválido")
+    }
+
+    private val viewModel: VisualizaNoticiaViewModel by viewModel { parametersOf(noticiaId) }
+    var quandoSelecionaMenuEdicao: () -> Unit = {}
+    var quandoFinalizaTela: () -> Unit = {}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        verificaIdDaNoticia()
+        buscaNoticiaSelecionada()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_visualiza_noticia, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.visualiza_noticia_menu, menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.visualiza_noticia_menu_edita -> quandoSelecionaMenuEdicao()
+            R.id.visualiza_noticia_menu_remove -> remove()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun buscaNoticiaSelecionada() {
+        viewModel.noticiaEncontrada.observe(this, Observer { noticiaEncontrada ->
+            noticiaEncontrada?.let {
+                preencheCampos(it)
+            }
+        })
+    }
+
+    private fun verificaIdDaNoticia() {
+        if (noticiaId == 0L) {
+            mostraErro(NOTICIA_NAO_ENCONTRADA)
+            quandoFinalizaTela()
+        }
+    }
+
+    private fun preencheCampos(noticia: Noticia) {
+        visualiza_noticia_titulo.text = noticia.titulo
+        visualiza_noticia_texto.text = noticia.texto
+    }
+
+    private fun remove() {
+        viewModel.remove().observe(this, Observer {
+            if (it.erro == null) {
+                quandoFinalizaTela()
+            } else {
+                mostraErro(MENSAGEM_FALHA_REMOCAO)
+            }
+        })
+    }
+}
